cdf07bfbfaf4dc36c595e8043688a763190a56f1
Arta Bytyqi <ab74418@ubt-uni.net>
2026-06-02 03:22:10 +0200

    korigjim

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import "./App.css";
 
-
 const root = ReactDOM.createRoot(
     document.getElementById("root")
 );
@@ -13,12 +12,4 @@ root.render(
     <BrowserRouter>
         <App />
     </BrowserRouter>
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
- (Improve Rooms UI)
 );
