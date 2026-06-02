58b004100b4bc78fc2f3c2d5f081ea841ab3c522
Arta Bytyqi <ab74418@ubt-uni.net>
2026-06-02 11:51:47 +0200

    zgjidhja e merge confilct

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-<<<<<<< HEAD
 import "./App.css";
-
-const root = ReactDOM.createRoot(
-    document.getElementById("root")
-);
-
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-=======
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
@@ -22,5 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <App />
         </BrowserRouter>
     </React.StrictMode>
->>>>>>> ff71e7c08ec89f10207f6e7922eafd567df7a224
 );
